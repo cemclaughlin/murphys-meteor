0cb566e182eb2cd0602d6ef5a2169ef44eb9de6c
Cristina McLaughlin <cemclaug@hawaii.edu>
2018-03-06 11:37:57 -0900

    commit

---
+++ app/imports/ui/pages/Murphys.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Mid from '../components/Mid';
+
+export default class Murphys extends React.Component {
+
+  render() {
+    return (
+        <div>
+          <Mid/>
+        </div>
+    );
+  }
+}
